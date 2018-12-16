1b407db9e6a5a53087807a1e77cc29c137d979a3
Ubuntu <ubuntu@ip-172-31-29-191.us-west-2.compute.internal>
2018-12-16 16:45:01 +0000

    dbl did a commit die

--- cogs/guilds.py
+++ cogs/guilds.py
@@ -7,25 +7,23 @@ class Guilds:
     def __init__(self, bot):
         self.bot = bot
 
-    async def update_guild_count(self):
+    async def update_guild_count(self, shard_id):
         await self.bot.change_presence(activity=discord.Activity(name=f'$help | {len(self.bot.guilds)} guilds', type=discord.ActivityType.listening))
-        payload = {'server_count': sum(1 for g in self.bot.guilds)}
-        await self.bot.session.request('POST', f'https://discordbots.org/api/bots/{self.bot.user.id}/stats', data=ujson.dumps(payload, ensure_ascii=True),
-        headers={'Content-Type': 'application/json', 'Authorization': self.bot.config.dbl})
         await self.bot.session.request('POST', f'https://botlist.space/api/bots/{self.bot.user.id}', data=ujson.dumps(payload, ensure_ascii=True),
         headers={'Content-Type': 'application/json', 'Authorization': self.bot.config.botlist})
-        await self.bot.session.request('POST', f'https://bots.discord.pw/api/bots/{self.bot.user.id}/stats', data=ujson.dumps(payload, ensure_ascii=True),
+        dPayload = {'guildCount': sum(1 for g in self.bot.guilds), 'shardCount': len(self.bot.shards), 'shardId': shard_id}
+        await self.bot.session.request('POST', f'https://discord.bots.gg/api/v1/bots/{self.bot.user.id}/stats', data=ujson.dumps(payload, ensure_ascii=True),
         headers={'Content-Type': 'application/json', 'Authorization': self.bot.config.dbots})
         await asyncio.sleep(1)
 
     async def on_ready(self):
-        await self.update_guild_count()
+        await self.update_guild_count(0)
    
     async def on_guild_join(self, guild):
-        await self.update_guild_count()
+        await self.update_guild_count(guild.shard_id)
     
     async def on_guild_remove(self, guild):
-        await self.update_guild_count()
+        await self.update_guild_count(guild.shard_id)
 
 def setup(bot):
     bot.add_cog(Guilds(bot))
